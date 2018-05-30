496637d2245676abbaee560d2054eee04a19778f
Unknown <unknown@example.com>
2018-05-31 00:30:59 +0700

    modal belum jadi

--- adminData/daftarPesanJoin.php
+++ adminData/daftarPesanJoin.php
@@ -197,5 +197,14 @@
   </div>
 </div>
 
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <form method="post">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="myModalLabel">Struct</h4>
+        </div>
 </body>
 </html>
