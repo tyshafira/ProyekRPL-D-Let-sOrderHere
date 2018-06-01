26127b9c88f909dc5a010163d39a1df4209d9d8f
Tya Shafira <Tya Shafira@DESKTOP-D27SP0D>
2018-06-01 09:59:26 -0700

    penambahan halaman footeer

--- index.php
+++ index.php
@@ -237,6 +237,37 @@ session_start();
   <?php endif; ?>
   <!-- / myorder -->
 
+  <!-- footer -->
+  <footer class="footer text-center">
+          <div class="footer-top">
+            <div class="row">
+              <div class="col-md-offset-3 col-md-6 text-center">
+                <div class="widget">
+                  <h4 class="widget-title">Let's Order Here</h4>
+                  <address>123 Iskandar Road<br>Aceh, Kode Pos 23117</address>
+                  <div class="social-list">
+                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                  </div>
+                  <p class="copyright clear-float">
+                    © Let's Order Here Theme. All Rights Reserved
+                    <div class="credits">
+                <!--
+                  All the links in the footer should remain intact.
+                  You can delete the links only if you purchased the pro version.
+                  Licensing information: https://bootstrapmade.com/license/
+                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Delicious
+                -->
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+              </div>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- / footer -->
+
   <!-- Modal login -->
     <div class="modal fade" id="myModal">
 			<div class="modal-dialog modal-sm">
