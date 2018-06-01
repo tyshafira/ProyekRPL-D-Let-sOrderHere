78fec15d95d5c58b23cafa3d49e8259af7fe2e72
nanda <nanda@DESKTOP-IEQNC6L>
2018-06-01 11:21:17 -0700

    tess push

--- adminData/adminjoinmodal.php
+++ adminData/adminjoinmodal.php
@@ -204,7 +204,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="myModalLabel">Struct</h4>
+          <h4 class="modal-title" id="myModalLabel">Struk</h4>
         </div>
         <div class="modal-body">
           <table id="data-pesanan" class="table table-striped">
